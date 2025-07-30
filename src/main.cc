268c3e477a44f9763038ebf482824fdacf46905f
Charles Huang <charles6huang@gmail.com>
2025-07-30 14:37:59 -0400

    moved makefile to root

--- src/main.cc
+++ src/main.cc
@@ -5,41 +5,47 @@
 #include <fstream>
 using namespace std;
 
-
-int main (int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     AsciiInterpreter v; // View subclass
     Controller c(&v);
     string initFile;
     string deck1, deck2 = "default.deck";
     bool testing = false;
-    for (int i = 1; i < argc; i++) {
-        if (string(argv[i]) == "-init") {
+    for (int i = 1; i < argc; i++)
+    {
+        if (string(argv[i]) == "-init")
+        {
             initFile = argv[++i];
         }
-        if(string(argv[i]) == "-deck1") {
+        if (string(argv[i]) == "-deck1")
+        {
             deck1 = argv[++i];
         }
-        if(string(argv[i]) == "-deck2") {
+        if (string(argv[i]) == "-deck2")
+        {
             deck2 = argv[++i];
         }
-        if(string(argv[i]) == "-testing"){
+        if (string(argv[i]) == "-testing")
+        {
             testing = true;
         }
     }
 
-    // since we are in src and want deck files to be in outer file add ../
-    deck1 = "../" + deck1;
-    deck2 = "../" + deck2;
+    // // since we are in src and want deck files to be in outer file add ../
+    // deck1 = "../" + deck1;
+    // deck2 = "../" + deck2;
 
-    if(!initFile.empty()) {
-            ifstream fin(initFile);
-            c.init(fin, deck1, deck2, testing);
-            c.play(fin);
-            c.play(cin);
+    if (!initFile.empty())
+    {
+        ifstream fin(initFile);
+        c.init(fin, deck1, deck2, testing);
+        c.play(fin);
+        c.play(cin);
     }
-    else {
+    else
+    {
         c.init(cin, deck1, deck2, testing);
         c.play(cin);
-
     }
 }
